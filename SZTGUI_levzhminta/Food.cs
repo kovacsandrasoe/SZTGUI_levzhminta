fd0befa3f8d95b772d63d4e09cf45c6227af282d
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-04-04 10:51:37 +0200

    01 vm és entityk elkészítése

---
+++ SZTGUI_levzhminta/Food.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZTGUI_levzhminta
+{
+    public class Food
+    {
+        public Food(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Food()
+        {
+            
+        }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public Food GetCopy()
+        {
+            return new Food(Name, Price);
+        }
+
+    }
+}
---
+++ SZTGUI_levzhminta/FoodViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZTGUI_levzhminta
+{
+    public class FoodViewModel
+    {
+        public ObservableCollection<Food> Foods { get; set; }
+
+        public ObservableCollection<Food> Basket { get; set; }
+
+        public FoodViewModel()
+        {
+            Foods = new ObservableCollection<Food>()
+            {
+                new Food("paprika", 500),
+                new Food("hagyma", 400),
+                new Food("zsír", 100),
+                new Food("marhahús", 3500),
+                new Food("burgonya", 1500),
+            };
+
+            Basket = new ObservableCollection<Food>()
+        }
+    }
+}
