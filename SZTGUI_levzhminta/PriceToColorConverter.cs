c61ef448c2fe8fb51e28f5ebbb0bcf427f59912a
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-04-04 11:17:43 +0200

    05 Szín converter

---
+++ SZTGUI_levzhminta/PriceToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SZTGUI_levzhminta
+{
+    public class PriceToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var price = (int)value;
+            if (price > 1000)
+            {
+                return Brushes.Red;
+            }
+            else
+            {
+                return Brushes.Black;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
