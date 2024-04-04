a9289544ec4a3c4f76835eb3a35dc51e31766f4a
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-04-04 11:11:57 +0200

    04 Commandok megvalósítása

--- SZTGUI_levzhminta/FoodViewModel.cs
+++ SZTGUI_levzhminta/FoodViewModel.cs
@@ -1,17 +1,56 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SZTGUI_levzhminta
 {
-    public class FoodViewModel
+    public class FoodViewModel : ObservableObject
     {
         public ObservableCollection<Food> Foods { get; set; }
 
         public ObservableCollection<Food> Basket { get; set; }
+
+        private Food selectedFood;
+        public Food SelectedFood
+        {
+            get
+            {
+                return selectedFood;
+            }
+            set
+            {
+                selectedFood = value;
+                OnPropertyChanged();
+                (AddCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+        private Food selectedBasket;
+
+        public Food SelectedBasket
+        {
+            get
+            {
+                return selectedBasket;
+            }
+            set
+            {
+                selectedBasket = value; 
+                OnPropertyChanged();
+                (DeleteCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+
+        public ICommand DeleteCommand { get; set; }
 
         public FoodViewModel()
         {
@@ -25,6 +64,16 @@ namespace SZTGUI_levzhminta
             };
 
             Basket = new ObservableCollection<Food>();
+
+            AddCommand = new RelayCommand(() =>
+            {
+                Basket.Add(SelectedFood.GetCopy());
+            }, () => SelectedFood != null);
+
+            DeleteCommand = new RelayCommand(() =>
+            {
+                Basket.Remove(SelectedBasket);
+            }, () => SelectedBasket != null);
         }
     }
 }
